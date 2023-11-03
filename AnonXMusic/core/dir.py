ed1a5a036d24c3544aa162589820f07dfe9facbb
Zaid <itsunknown122@gmail.com>
2023-11-03 16:56:44 +0530

    Fix directory(#13)

--- AnonXMusic/core/dir.py
+++ AnonXMusic/core/dir.py
@@ -4,14 +4,8 @@ import logging
 from os import listdir, mkdir
 
 def dirr():
-    assets_folder = "assets"
     downloads_folder = "downloads"
     cache_folder = "cache"
-
-    if assets_folder not in listdir():
-        logging.warning(f"{assets_folder} Folder not Found. Please clone repository again.")
-        sys.exit()
-
     for file in os.listdir():
         if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
             os.remove(file)
