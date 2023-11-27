8cb75f578a9c39567a4907e4ab0965aa1bfa15b5
NEO xD <122731828+neolol3@users.noreply.github.com>
2023-11-27 22:50:38 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -5,19 +5,19 @@ from AnonXMusic import app
 
 
 def start_panel(_):
-    buttons = [
+    return [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
-    return buttons
 
 
 def private_panel(_):
-    buttons = [
+    return [
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
@@ -25,12 +25,15 @@ def private_panel(_):
             )
         ],
         [
+            InlineKeyboardButton(
+                text=_["S_B_4"], callback_data="settings_back_helper"
+            )
+        ],
+        [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
-    return buttons
